eed5502e9faadd9c0dcf71f08f75bf1a9f97c512
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2020-09-27 10:19:43 -0300

    Asp.NET Core - Uma webapp usando o padrão MVC - Aplicação Inicial

---
+++ ListaLeitura.App/Program.cs
@@ -0,0 +1,25 @@
+﻿using ListaLeitura.App.Negocio;
+using ListaLeitura.App.Repositorio;
+using System;
+
+namespace ListaLeitura.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            ImprimeLista(_repo.ParaLer);
+            ImprimeLista(_repo.Lendo);
+            ImprimeLista(_repo.Lidos);
+
+            Console.ReadLine();
+        }
+
+        static void ImprimeLista(ListaDeLeitura lista)
+        {
+            Console.WriteLine(lista);
+        }
+    }
+}
